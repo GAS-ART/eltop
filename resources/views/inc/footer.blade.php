7fa8a08d3fefe96aed3beff141a1a4607f27ee88
GAS-ART <temoha1386@gmail.com>
2023-04-26 08:52:40 +0300

    Added map iframe, added static fonts

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,5 +2,9 @@
 <footer class="footer">
    <div class="footer__container">
       FOOTER FOOTER FOOTER
+      <iframe
+         src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1924.6411716619882!2d32.226148231872834!3d48.487839297754846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDjCsDI5JzE0LjIiTiAzMsKwMTMnMzguNyJF!5e0!3m2!1sru!2sua!4v1682486441321!5m2!1sru!2sua"
+         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+         referrerpolicy="no-referrer-when-downgrade"></iframe>
    </div>
 </footer>
