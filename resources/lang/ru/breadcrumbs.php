d1d542e8961f10e2fccf55e1ef59b9b31b53cbd1
GAS-ART <temoha1386@gmail.com>
2023-05-08 17:40:30 +0300

    added single product page

--- resources/lang/ru/breadcrumbs.php
+++ resources/lang/ru/breadcrumbs.php
@@ -5,6 +5,7 @@ return [
       'nibe' => [
         'catalog' => 'Тепловые насосы Nibe',
         's1255' => 'NIBE S1255',
+        'nibe-s1255-6kw-400V' => 'nibe-s1255-6kw-400V',
       ],
       'liveheat' => [
         'catalog' => 'Тепловые насосы Liveheat',
--- resources/lang/ua/breadcrumbs.php
+++ resources/lang/ua/breadcrumbs.php
@@ -5,6 +5,7 @@ return [
       'nibe' => [
         'catalog' => 'Теплові насоси Nibe',
         's1255' => 'NIBE S1255',
+        'nibe-s1255-6kw-400V' => 'nibe-s1255-6kw-400V',
       ],
       'liveheat' => [
         'catalog' => 'Теплові насоси Liveheat',
