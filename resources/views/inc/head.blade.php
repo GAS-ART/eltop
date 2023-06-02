8337675a2c44bce96feb9a83e0fa0817d3561d13
GAS-ART <temoha1386@gmail.com>
2023-06-02 16:56:58 +0300

    Added GA tag

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -22,6 +22,15 @@
 
    @vite(['resources/scss/style.scss'])
 
-   <!-- Global site tag (gtag.js) - Google Analytics -->
+   <!-- Google tag (gtag.js) -->
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-5ZX6DKKST1"></script>
+   <script>
+      window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5ZX6DKKST1');
+   </script>
+
 
 </head>
