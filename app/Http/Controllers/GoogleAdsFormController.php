54771aee4c890c769fc60c207752f3d403fb2c64
GAS-ART <temoha1386@gmail.com>
2023-07-25 10:25:22 +0300

    Fixed google ads form controller

--- app/Http/Controllers/GoogleAdsFormController.php
+++ app/Http/Controllers/GoogleAdsFormController.php
@@ -8,14 +8,21 @@ use Mail;
 class GoogleAdsFormController extends Controller
 {
     public function sendDataToEmail (Request $req){
-        $name = $req->input('name');
-        $phone = $req->input('phone');
-        $city = $req->input('city');
-        $houseArea = $req->input('houseArea');
-        $people = $req->input('people');
+        $raw = $req->getContent();
+        $data = json_decode($raw, true);
+
+        if ($data['google_key'] === '26ahAmim'){
+        $name = $data['user_column_data'][0]['string_value'];
+        $phone = $data['user_column_data'][1]['string_value'];
+        $city = $data['user_column_data'][2]['string_value'];
+        $houseArea = $data['user_column_data'][3]['string_value'];
+        $people = $data['user_column_data'][4]['string_value'];
 
         Mail::send(['html' => 'google_ads_form'], ['name' => $name, 'phone' => $phone, 'city' => $city, 'houseArea' => $houseArea, 'people' => $people,], function($message) {
             $message->to('temoha1386@gmail.com')->subject('СООБЩЕНИЕ ИЗ ФОРМЫ GOOGLE');
         });
+        return response()->json(['success' => true]);
+    }
+        return response()->json(['success' => false, 'error' => 'Invalid data or key'], 400);
     }
 }
