495d7ff265e6df9c134a948012e2bd88618659e6
GAS-ART <temoha1386@gmail.com>
2023-07-25 09:39:11 +0300

    Added processing of post requests from advertising Google forms

---
+++ app/Http/Controllers/GoogleAdsFormController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+
+class GoogleAdsFormController extends Controller
+{
+    public function sendDataToEmail ($req){
+        $name = $req->input('name');
+        $phone = $req->input('phone');
+        $city = $req->input('city');
+        $houseArea = $req->input('houseArea');
+        $people = $req->input('people');
+
+        Mail::send(['html' => 'google_ads_form'], ['name' => $name, 'phone' => $phone, 'city' => $city, 'houseArea' => $houseArea, 'people' => $people,], function($message) {
+            $message->to('temoha1386@gmail.com')->subject('СООБЩЕНИЕ ИЗ ФОРМЫ GOOGLE');
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleAdsFormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/locale/{locale}',  'App\Http\Controllers\LanguagesController@changeLocale')->name('locale');
 Route::post('/send-main-form', 'App\Http\Controllers\sendController@submit')->name('sendMainForm');
+Route::post('/gaf', [GoogleAdsFormController::class], 'sendDataToEmail')->name('google_ads_form');
 
 Route::get('/politika_konfidencialnosti', function () {return view('politika_konfidencialnosti');})->name('politika_konfidencialnosti');
 
